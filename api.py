0ae2bf70857f1e01db17b3bc3330dc60d037765f
demono5060 <demono5060@mail.ru>
2023-11-14 16:31:03 +0700

    Error handling for an invalid API key has been added.

--- api.py
+++ api.py
@@ -7,8 +7,12 @@ class Api:
         self.url = url
         self.api_key = api_key
 
-    def find_address(self, address: str) -> dict:
-        return json.loads(requests.post(self.url, json={'query': address},
-                                        headers={'Authorization': 'Token ' + self.api_key,
-                                                 'Content-Type': 'application/json',
-                                                 'Accept': 'application/json'}).text).get('suggestions')
+    def find_address(self, address: str) -> dict | None:
+        r = requests.post(self.url, json={'query': address},
+                          headers={'Authorization': 'Token ' + self.api_key,
+                                   'Content-Type': 'application/json',
+                                   'Accept': 'application/json'})
+        if r.status_code == 200:
+            return json.loads(r.text).get('suggestions')
+        elif r.status_code == 403:
+            raise 'Wrong api-key'
